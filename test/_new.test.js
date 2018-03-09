51a85fbddf4a192d9d22e144f598423a472a95ed
zhanghl-k <zhanghl-k@glodon.com>
2018-03-09 18:06:28 +0800

    'init'

---
+++ test/_new.test.js
@@ -0,0 +1,13 @@
+/* eslint no-shadow: 0 */
+
+
+var test = require('tap').test;
+var ss = require('../');
+
+test('addToMean', function (t) {
+    t.test('can get add a single value to a mean', function (t) {
+        t.equal(true, true);
+        t.end();
+    });
+    t.end();
+});
